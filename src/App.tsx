5e53b45911957bcbe19bf07d0fc1c14c06a1affa
hakan.taskirmaz <hakan.taskirmaz@minbzk.nl>
2022-03-09 22:10:31 +0100

    added quantic image

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,15 @@ import logo from "./components/img/logo.png"
 import titano from "./components/img/titano.png"
 import libero from "./components/img/libero.png"
 import fino from "./components/img/fino.png"
+import quantic from "./components/img/quantic.png"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        {/* <div className="left">
-          <CryptoContainer token={"libero-financial"} img={libero} />
-        </div> */}
-        <CryptoContainer token={"quantic"} img={logo} />
+        <CryptoContainer token={"quantic"} img={quantic} />
         <CryptoContainer token={"titano"} img={titano} />
+        <CryptoContainer token={"libero-financial"} img={libero} />
         <CryptoContainer token={"fino-dao"} img={fino} />
       </div>
       <ParticleWrapper  />
